3960543db20e87f1cfadcbc7e6174ab45f6a5201
sinpor <mymail33333@163.com>
2023-02-21 00:46:46 +0800

    Exclude

--- 00043-easy-exclude.ts
+++ 00043-easy-exclude.ts
@@ -9,4 +9,13 @@ type cases = [
 
 
 // ============= Your Code Here =============
-type MyExclude<T, U> = 
+type MyExclude<T, U> = T extends U ? never : T;
+
+
+/**
+ * refrence https://juejin.cn/book/7047524421182947366/section/7048282387825819687
+ * 
+ * 条件类型的分布性
+ * 
+ * 当联合类型作为条件类型的左侧，会将该类型的每个类型执行条件类型，然后合并成新的联合类型。
+ */
